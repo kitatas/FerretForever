b3fab9538676de7442aae68bfbc2b62ea30afe47
kitatas <315kitatas10703@gmail.com>
2022-01-19 00:22:40 +0900

    add: BaseButtonView

---
+++ Assets/Ferret/Scripts/Common/Presentation/View/BaseButtonView.cs
@@ -0,0 +1,38 @@
+using System;
+using DG.Tweening;
+using Doozy.Runtime.UIManager.Components;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Ferret.Common.Presentation.View
+{
+    [RequireComponent(typeof(UIButton))]
+    public class BaseButtonView : MonoBehaviour
+    {
+        public Action push;
+
+        private const float _animationTime = 0.1f;
+
+        public void Init()
+        {
+            var button = GetComponent<UIButton>();
+            var scale = transform.localScale;
+
+            push += () =>
+            {
+                DOTween.Sequence()
+                    .Append(button.rectTransform
+                        .DOScale(scale * 0.8f, _animationTime))
+                    .Append(button.rectTransform
+                        .DOScale(scale, _animationTime))
+                    .SetLink(gameObject);
+            };
+
+            button
+                .OnPointerDownAsObservable()
+                .Subscribe(_ => push?.Invoke())
+                .AddTo(this);
+        }
+    }
+}
