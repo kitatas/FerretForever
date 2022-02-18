65e0c9e3e47f49f9b0c707807f4abecddafd5ff8
kitatas <315kitatas10703@gmail.com>
2022-02-18 23:51:59 +0900

    add: ハイスコア更新時の演出制御

--- Assets/Ferret/Scripts/OutGame/Presentation/View/RecordView.cs
+++ Assets/Ferret/Scripts/OutGame/Presentation/View/RecordView.cs
@@ -8,6 +8,7 @@ namespace Ferret.OutGame.Presentation.View
     public sealed class RecordView : MonoBehaviour
     {
         [SerializeField] private GameObject newRecordLabel = default;
+        [SerializeField] private GameObject background = default;
         [SerializeField] private TextMeshProUGUI highScore = default;
         [SerializeField] private TextMeshProUGUI score = default;
 
@@ -16,7 +17,9 @@ namespace Ferret.OutGame.Presentation.View
             var highScoreValue = highRecord.score;
             var scoreValue = currentRecord.score;
 
-            newRecordLabel.SetActive(scoreValue.Equal(highScoreValue));
+            var isNewRecord = scoreValue.Equal(highScoreValue);
+            newRecordLabel.SetActive(isNewRecord);
+            background.SetActive(isNewRecord);
 
             highScore.text = $"{highScoreValue.ToString("F2")}";
             score.text = $"{scoreValue.ToString("F2")}";
