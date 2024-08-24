298e78329adefcf2b9c6ff70cf3ee2006c2e74e6
kitatas <315kitatas10703@gmail.com>
2024-08-24 23:35:33 +0900

    fix: external url

--- Assets/Ferret/Scripts/Common/Application/Const.cs
+++ Assets/Ferret/Scripts/Common/Application/Const.cs
@@ -26,7 +26,7 @@ namespace Ferret.Common
 
     public sealed class WebviewConfig
     {
-        public const string URL_BASE = "https://kitatas.github.io/FerretForever/";
+        public const string URL_BASE = "https://kitatas.github.io/games/ferret_forever/";
         public const string URL_CREDIT = URL_BASE + "credit";
         public const string URL_LICENSE = URL_BASE + "license";
         public const string URL_POLICY = URL_BASE + "policy";
